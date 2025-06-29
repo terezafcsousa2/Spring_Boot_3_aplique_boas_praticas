bfb057eca3418445c62668928d4af79f44d7ef4a
Terezinha Freire C Sousa <terezinhafcs@id.uff.br>
2025-06-29 18:28:15 -0300

    Add files via upload

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,68 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    private static final Logger log = LoggerFactory.getLogger(TokenService.class);
+    private static final String ISSUER = "API Voll.med";
+
+
+
+    public String gerarToken(Usuario usuario){
+
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer(ISSUER)
+                    .withSubject(usuario.getLogin())
+                    //.withClaim("id", usuario.getId())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            //exception.printStackTrace();
+            throw new RuntimeException("Token JWT inválido ou expirado" + tokenJWT, exception);
+        }
+    }
+
+
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(48).toInstant(ZoneOffset.of("-03:00"));//criar a data de expiração conforme o horário do Brasil
+
+    }
+
+}
